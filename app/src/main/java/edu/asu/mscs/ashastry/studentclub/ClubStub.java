4fdf4735f5e2c4d67a6a224c44ad77838ab75761
aneesh26 <aneesh26@gmail.com>
2015-04-06 04:14:05 -0700

    Implemented JSON RPC between the android app and the Java Server.
    Included the Server code within the StudentClub directory
    
    Android App can be accessed using Android Studio and the Server code can be accessed using any IDE (Eclipse, NetBeans or AndroidStudio)

---
+++ app/src/main/java/edu/asu/mscs/ashastry/studentclub/ClubStub.java
@@ -0,0 +1,192 @@
+package edu.asu.mscs.ashastry.studentclub;
+
+/**
+ * Copyright 2015 Aneesh Shastry
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Purpose:
+ *
+ * @author : Aneesh Shastry  mailto:ashastry@asu.edu
+ *           MS Computer Science, CIDSE, IAFSE, Arizona State University
+ * @version : February 9, 2015
+ */
+
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.net.ConnectException;
+import java.net.URL;
+
+
+public class ClubStub {
+    int errorF = 0;
+    private JsonRpcRequestViaHttp server;
+    private static int id=0;
+    ClubStub(URL url,int errorFlag){
+        server = new JsonRpcRequestViaHttp(url);
+        this.errorF = errorFlag;
+    }
+
+    public String getAboutUs(){
+        String res = null;
+
+        try{
+            JSONArray ja = new JSONArray();
+          //  ja.put(0,left).put(1,right);
+            String callStr = this.packageCalcCall("getAboutUs",ja);
+            android.util.Log.d("Client --> Server",callStr);
+            System.out.println("Before server call");
+            String retStr = server.call(callStr);
+            System.out.println("After server call");
+            android.util.Log.d("Server --> Client",retStr);
+            JSONObject result = new JSONObject(retStr);
+            res = result.optString("result");
+
+        }
+        catch(ConnectException ex1){
+            errorF = 1;
+        }
+        catch(Exception ex){
+
+            android.util.Log.d(this.getClass().getSimpleName(),"Error with JSON add method");
+            ex.printStackTrace();
+        }
+        return res;
+    }
+
+
+
+    private String packageCalcCall(String oper, JSONArray args){
+        String ret = "";
+        try{
+            JSONObject jsonObj = new JSONObject();
+            jsonObj.put("jsonrpc","2.0");
+            jsonObj.put("method",oper);
+            jsonObj.put("id", ++id);
+            String almost = jsonObj.toString();
+            String toInsert = null;
+            if(args == null){
+                toInsert = ",\"params\":[]";
+            } else{
+                toInsert = ",\"params\":" + args.toString();
+            }
+            String begin = almost.substring(0,almost.length()-1);
+            String end = almost.substring(almost.length()-1);
+            ret = begin + toInsert + end;
+        }
+        catch(Exception ex){
+            android.util.Log.d(this.getClass().getSimpleName(),"Error with packageCalcCall method");
+
+        }
+        return ret;
+    }
+
+
+ /*
+    public double add(double left, double right) {
+        double res = 0.0;
+        try{
+            JSONArray ja = new JSONArray();
+            ja.put(0,left).put(1,right);
+            String callStr = this.packageCalcCall("add",ja);
+            android.util.Log.d("Client --> Server",callStr);
+            String retStr = server.call(callStr);
+            android.util.Log.d("Server --> Client",retStr);
+            JSONObject result = new JSONObject(retStr);
+            res = result.optDouble("result");
+
+        }
+        catch(ConnectException ex1){
+            errorF = 1;
+        }
+        catch(Exception ex){
+
+            android.util.Log.d(this.getClass().getSimpleName(),"Error with JSON add method");
+        }
+        return res;
+    }
+    public double sub(double left, double right){
+        double res = 0.0;
+        try{
+            JSONArray ja = new JSONArray();
+            ja.put(0,left).put(1,right);
+            String callStr = this.packageCalcCall("subtract",ja);
+            android.util.Log.d("Client --> Server",callStr);
+            String retStr = server.call(callStr);
+            android.util.Log.d("Server --> Client",retStr);
+            JSONObject result = new JSONObject(retStr);
+            res = result.optDouble("result");
+
+        }
+        catch(ConnectException ex1){
+            errorF = 1;
+        }
+        catch(Exception ex){
+            android.util.Log.e("Sub","Error in subtract call function");
+
+        }
+
+        return res;
+
+    }
+    public double mul(double left, double right){
+        double res = 0.0;
+        try{
+            JSONArray ja = new JSONArray();
+            ja.put(0,left).put(1,right);
+
+            String callStr = this.packageCalcCall("multiply",ja);
+            android.util.Log.d("Client --> Server",callStr);
+            String retStr = server.call(callStr);
+            android.util.Log.d("Server --> Client",retStr);
+            JSONObject result = new JSONObject(retStr);
+            res = result.optDouble("result");
+
+
+        }
+        catch(ConnectException ex1){
+            errorF = 1;
+        }
+        catch(Exception ex){
+            android.util.Log.e("Sub","Error in multiply call function");
+
+        }
+        return res;
+    }
+    public double div(double left, double right){
+        double res = 0.0;
+        try{
+            JSONArray ja = new JSONArray();
+            ja.put(0,left).put(1,right);
+            String callStr = this.packageCalcCall("divide",ja);
+            android.util.Log.d("Client --> Server",callStr);
+            String retStr = server.call(callStr);
+            android.util.Log.d("Server --> Client",retStr);
+            JSONObject result = new JSONObject(retStr);
+            res = result.optDouble("result");
+
+        }
+        catch(ConnectException ex1){
+            errorF = 1;
+        }
+        catch(Exception ex){
+            android.util.Log.e("Sub", "Error in divide call function");
+
+        }
+        return res;
+    }
+*/
+
+}
--- app/src/main/java/edu/asu/mscs/ashastry/studentclub/HomeActivity.java
+++ app/src/main/java/edu/asu/mscs/ashastry/studentclub/HomeActivity.java
@@ -1,12 +1,25 @@
 package edu.asu.mscs.ashastry.studentclub;
 
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.AsyncTask;
+import android.os.Environment;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.app.ListActivity;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.text.NumberFormat;
 import java.util.ArrayList;
 
 
@@ -14,6 +27,7 @@ public class HomeActivity extends ListActivity {
 
     ArrayList<String> waypointList = new ArrayList<String>();
     ArrayAdapter<String> adapter;
+    String result;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,31 +43,43 @@ public class HomeActivity extends ListActivity {
 
         adapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1, waypointList);
 
-
-        //adding a new commen line
-
-        // updating one of the files
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        //adapter.notifyDataSetChanged();
+        setListAdapter(adapter);
 
     }
 
+
+    protected void onListItemClick(ListView l, View v1, int position, long id) {
+        String item = (String) getListAdapter().getItem(position);
+
+        if(item.equals("Events")){
+            Toast.makeText(HomeActivity.this, "Events Clicked", Toast.LENGTH_SHORT).show();
+        }else if(item.equals("News")){
+            Toast.makeText(HomeActivity.this, "News Clicked", Toast.LENGTH_SHORT).show();
+        }else if(item.equals("facebook page")){
+            Toast.makeText(HomeActivity.this, "FB Clicked", Toast.LENGTH_SHORT).show();
+           startActivity(getOpenFacebookIntent(this.getApplicationContext()));
+        }else if(item.equals("F.A.Qs")){
+            Toast.makeText(HomeActivity.this, "FAQ Clicked", Toast.LENGTH_SHORT).show();
+        }else if(item.equals("About Us")){
+            getAboutUs();
+
+          //  Toast.makeText(HomeActivity.this, this.result, Toast.LENGTH_LONG).show();
+        }else if(item.equals("Settings")){
+            Toast.makeText(HomeActivity.this, "Settings Clicked", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+
+    public static Intent getOpenFacebookIntent(Context context) {
+
+        try {
+            context.getPackageManager().getPackageInfo("com.facebook.katana", 0);
+            return new Intent(Intent.ACTION_VIEW, Uri.parse("fb://profile/152880021441864"));
+        } catch (Exception e) {
+            return new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.facebook.com/isa.asu"));
+        }
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -80,4 +106,101 @@ public class HomeActivity extends ListActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+
+    public void getAboutUs(){
+        try {
+            URL URLOut = new URL(this.getString(R.string.url_string));
+            MyTaskParams mp = new MyTaskParams(URLOut, "getAboutUs");
+            AsyncCalc ac = (AsyncCalc) new AsyncCalc().execute(mp);
+        }
+
+        catch (MalformedURLException m1){
+            android.util.Log.d("Error in URL","");
+        }
+
+        catch (Exception ex) {
+            android.util.Log.d(this.getClass().getSimpleName(), "Error connecting to the Server");
+        }
+    }
+
+
+    private static class MyTaskParams {
+        URL newURL;
+        String operatorV;
+
+
+        MyTaskParams(URL url, String op) {
+            this.newURL = url;
+            this.operatorV = op;
+
+        }
+
+        public URL getNewURL() {
+            return newURL;
+        }
+
+        public void setNewURL(URL newURL) {
+            this.newURL = newURL;
+        }
+
+        public String getOperatorV() {
+            return operatorV;
+        }
+
+        public void setOperatorV(String operatorV) {
+            this.operatorV = operatorV;
+        }
+    }
+    private class AsyncCalc extends AsyncTask<MyTaskParams, Integer, String> {
+
+        double val = 0.0;
+        String returnValue = null;
+        int errorFlag = 0;
+        protected String doInBackground(MyTaskParams... mtp) {
+            try{
+                ClubStub calc = new ClubStub(mtp[0].getNewURL(),errorFlag);
+                if(errorFlag == 1){
+                   android.util.Log.d(this.getClass().getSimpleName(),"Error with ClubStub class");
+                   // ((TextView)HomeActivity.this.findViewById(R.id.messageView)).setText("Cannot connect to the server");
+                }
+                String oper = mtp[0].getOperatorV();
+                //double leftOp = exp1;
+                //double rightOp = exp2;
+                if("getAboutUs".equals(oper)){
+                    returnValue = calc.getAboutUs();
+                }
+              /*  else if("Subtract".equals(oper)){
+                    val = calc.sub(leftOp, rightOp);
+                }
+                else if("Multiply".equals(oper)){
+                    val = calc.mul(leftOp, rightOp);
+                }
+                else if("Divide".equals(oper)){
+                    val = calc.div(leftOp, rightOp);
+                }*/
+            }
+
+
+            catch(Exception ex){
+                ex.printStackTrace();
+           //     ((TextView)findViewById(R.id.messageView)).setText("Error Creating Async task");
+             //   ((TextView)findViewById(R.id.displayNum)).setText("");
+            }
+            return returnValue;
+        }
+
+        protected void onPostExecute(String res){
+            NumberFormat nf = NumberFormat.getInstance();
+            nf.setMaximumFractionDigits(2);
+            android.util.Log.d(this.getClass().getSimpleName(),res);
+            HomeActivity.this.result = res;
+            Toast.makeText(HomeActivity.this, res, Toast.LENGTH_LONG).show();
+            //((TextView)findViewById(R.id.displayNum)).setText(nf.format(res));
+            //exp1 = 0;
+           // exp2 = 0;
+           // decExp = 0;
+        }
+    }
+
 }
